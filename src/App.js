0b9af36c5fae5c981e5a1814918ea1212e92caf6
Shubham Dutta <62681943+Shubhamdutta2000@users.noreply.github.com>
2020-10-06 00:07:07 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Footer from "./components/footer";
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<p>Welcome to QA Frontend</p>
+
+			</header>
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
